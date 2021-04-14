b6eb2b40753fb40bb4c2994757124fbb8c8bd6e8
NicoMunoz909 <nicolas.munoz@outlook.es>
2021-04-14 09:28:44 -0400

    Change edit book implementation

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let myLibrary = [];
-let bookCardList = []
 
 function Book(title,author,genre,pages,read) {
   this.title = title,
@@ -7,10 +6,7 @@ function Book(title,author,genre,pages,read) {
   this.genre = genre,
   this.pages = pages,
   this.read = read
-}
-
-function addBookToLibrary(title,author,genre,pages,read) {
-  myLibrary.push(new Book(title,author,genre,pages,read))
+  this.bookCard = createBookCard(this);
 }
 
 function createBookCard(book) {
@@ -60,6 +56,10 @@ function createBookCard(book) {
   return element
 }
 
+function addBookToLibrary(title,author,genre,pages,read) {
+  myLibrary.push(new Book(title,author,genre,pages,read))
+}
+
 function updateLibraryInfo() {
   const booksNumber = document.getElementById('books-number');
   booksNumber.textContent = 'Number of Books: ' + myLibrary.length
@@ -75,60 +75,22 @@ function updateLibraryInfo() {
   booksRead.textContent = 'Books read: '+reads;
 }
 
-
-function insertBook(book) {
-  const bookCard = createBookCard(book);
-  bookCardList.push(bookCard)
-  const container = document.getElementById("book-list");
-  const addBookBtn = document.getElementById("add-book-btn")
-  container.appendChild(bookCard);
+function showBooks() {
+  myLibrary.forEach(book => insertBookCard(book))
 }
 
-function editBook() {
-  let id = document.getElementById('identification').textContent;
-  const bookCard = document.getElementById(id);
-  const book = myLibrary[myLibrary.findIndex(book => book.title == id)];
-  
-  const title = document.getElementById('edit-title').value;
-  bookCard.children[0].textContent = title;
-  book.title = title;
-  bookCard.id = title;
-  id = title;
-
-  const author = document.getElementById('edit-author').value;
-  bookCard.children[1].textContent  = author;
-  book.author = author;
-
-  const genre = document.getElementById('edit-genre').value;
-  bookCard.children[2].textContent  = genre;
-  book.genre = genre;
-
-  const pages = document.getElementById('edit-pages').value;
-  bookCard.children[3].textContent  = "Pages: "+pages;
-  book.pages = parseInt(pages);
-
-  if (document.getElementById('edit-read').checked) {
-    bookCard.children[4].textContent  = 'Read';
-    book.read = true;
-  } else {
-    bookCard.children[4].textContent  = 'Not Read';
-    book.read = false;
-  }
-  updateLibraryInfo();
-  closeEditBookForm();
-  
+function insertBookCard(book) {
+  const container = document.getElementById("book-list");
+  container.appendChild(book.bookCard);
 }
 
 function deleteBook() {
   myLibrary.splice(myLibrary.indexOf(this),1)
   const bookList = document.getElementById("book-list")
-  bookList.removeChild(document.getElementById(this.title))
+  bookList.removeChild(this.bookCard)
   updateLibraryInfo();
 }
 
-function showBooks() {
-  myLibrary.forEach(book => insertBook(book))
-}
 
 function openAddBookForm() {
   const form = document.getElementById("add-book-form");
@@ -186,8 +148,11 @@ function openEditBookForm() {
   } else {
     document.getElementById('edit-not-read').checked = true;
   }
+  const editButton = document.getElementById('editform-btn');
+  editButton.dataset.bookIndex = myLibrary.indexOf(this);
   form.style.opacity = 1
   form.style.display = "block"
+  
 }
 
 function closeEditBookForm() {
@@ -199,7 +164,39 @@ function closeEditBookForm() {
   form.style.display = "none"
 }
 
+function editBookCallback() {
+  const editButton = document.getElementById('editform-btn');
+  const index = editButton.dataset.bookIndex;
+  const book = myLibrary[index];
+  const bookCard = book.bookCard;
+  
+  const title = document.getElementById('edit-title').value;
+  bookCard.children[0].textContent = title;
+  book.title = title;
 
+  const author = document.getElementById('edit-author').value;
+  bookCard.children[1].textContent  = author;
+  book.author = author;
+
+  const genre = document.getElementById('edit-genre').value;
+  bookCard.children[2].textContent  = genre;
+  book.genre = genre;
+
+  const pages = document.getElementById('edit-pages').value;
+  bookCard.children[3].textContent  = "Pages: "+pages;
+  book.pages = parseInt(pages);
+
+  if (document.getElementById('edit-read').checked) {
+    bookCard.children[4].textContent  = 'Read';
+    book.read = true;
+  } else {
+    bookCard.children[4].textContent  = 'Not Read';
+    book.read = false;
+  }
+  updateLibraryInfo();
+  closeEditBookForm();
+  
+}
 
 const addBookBtn = document.getElementById("add-book-btn")
 addBookBtn.addEventListener('click',openAddBookForm)
